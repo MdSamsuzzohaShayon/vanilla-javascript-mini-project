240a5f5c19344a65ab69d39182e6dd6bb4ad2392
Md Shayon <mdshayon0@gmail.com>
2021-06-21 08:18:39 +0600

    parsing csv data. another way

--- data-visualization/fetch-data/main.js
+++ data-visualization/fetch-data/main.js
@@ -1,9 +1,28 @@
 const csvUrl = "https://gist.githubusercontent.com/MdSamsuzzohaShayon/8a2aed8d276f038aee513dd0a5bea5f8/raw/07f62137afbf6c9210b72d4fa728b2009ff788b3/colorsData.csv";
+const messageContainer = document.getElementById('message');
 
-
+// THIS IS ONE WAY OF PARSING CSV DATA 
+/*
 const fetchText = async (url)=>{
     const response = await fetch(url);
     return await response.text();
 
 }
-fetchText(csvUrl).then(text=>console.log(text));
+fetchText(csvUrl).then(text=>{
+    const data = d3.csvParse(text);
+    let message = data.length + " - Rows; " + data.columns.length + " - Columns; " + Math.round(text.length / 1024) + " - kB";
+    // console.log(data.length + " - Rows");
+    // console.log(data.columns.length + " - Columns");
+    // console.log(Math.round(text.length / 1024) + " - kB");
+    console.log(message);
+    messageContainer.textContent = message;
+});
+*/
+
+
+// ANOTHER WAY TO PARSING CSV DATA 
+d3.csv(csvUrl).then(data=>{
+    const message = data.length + " - Rows; " + data.columns.length + " - Columns; " + Math.round(d3.csvFormat(data).length / 1024) + " - kB";
+    console.log(message);
+    messageContainer.textContent = message;
+});
